fabd83784fe1ee2a63664269e823c9db096b8151
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-08 14:50:01 -0300

    menor e maior altura

--- Altura15Pessoas.ConsoleApp/Program.cs
+++ Altura15Pessoas.ConsoleApp/Program.cs
@@ -6,7 +6,26 @@ namespace Altura15Pessoas.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] alturas = new double[3];
+            double maior = 0;
+            double menor = 0;
+
+            for (int i = 0; i < alturas.Length; i++)
+            {
+                Console.Write("Digite a altura da pessoa "+i+": ");
+                alturas[i] = Convert.ToDouble(Console.ReadLine());
+
+                if (alturas[i]> maior)
+                {
+                    maior = alturas[i];
+                }
+                else
+                {
+                    menor = alturas[i];
+                }
+            }
+            Console.WriteLine("A maior altura é: "+maior);
+            Console.WriteLine("A menor altura é: "+menor);
         }
     }
 }
