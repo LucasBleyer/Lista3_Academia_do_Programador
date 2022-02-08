857e315177ee8490742c5314d8ac7bd8fefd2b02
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-08 14:58:02 -0300

    files

---
+++ Altura15Pessoas.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MenorMaiorAltura.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] alturas = new double[3];
+            double maior = 0;
+            double menor = 99999999;
+
+            for (int i = 0; i < alturas.Length; i++)
+            {
+                Console.Write("Digite a altura da pessoa "+i+": ");
+                alturas[i] = Convert.ToDouble(Console.ReadLine());
+
+                if (alturas[i]> maior)
+                {
+                    maior = alturas[i];
+                }
+                else
+                {
+                    menor = alturas[i];
+                }
+            }
+            Console.WriteLine("A maior altura é: "+maior);
+            Console.WriteLine("A menor altura é: "+menor);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Impares100To200.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Impares100To200.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
