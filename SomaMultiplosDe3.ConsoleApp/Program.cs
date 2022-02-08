0fdd973d4b11aa4c8cdef5c7504290aece4afaf1
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-08 16:20:48 -0300

    add soma mult 3 até 500

---
+++ SomaMultiplosDe3.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SomaMultiplosDe3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 0; i < 500; i++)
+            {
+                int resto_par = i % 2;
+                int resto_impar = i % 3;
+
+                if (resto_par != 0 && resto_impar == 0)
+                {
+                    soma = soma + i;
+                }
+            }
+            Console.Write("Soma dos multiplos de 3 do 1 a 500: "+soma);
+            Console.ReadLine();
+        }
+    }
+}
