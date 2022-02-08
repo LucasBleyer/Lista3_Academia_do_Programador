f475ef556cea3f629db3f67a9e02952a6d5b1999
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-08 16:37:00 -0300

    add tabuada

--- Tabuada.ConsoleApp/Program.cs
+++ Tabuada.ConsoleApp/Program.cs
@@ -6,7 +6,14 @@ namespace Tabuada.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Digite um número para Tabuada: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int soma = i * n;
+                Console.WriteLine(i+" x "+n+" = "+soma);
+            }
         }
     }
 }
