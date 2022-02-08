778a8d77c446ac32bb15d8b089aa4793350d8102
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-08 13:55:52 -0300

    Add project files.

---
+++ Altura15Pessoas.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Altura15Pessoas.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
